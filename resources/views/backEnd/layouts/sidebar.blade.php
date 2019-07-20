bed7f456c1df46eb60165ed653fc474eeeb160fe
AhmadTanvir <atasiddiq@gmail.com>
2019-07-20 06:33:21 -0700

    Update sidebar.blade.php

--- resources/views/backEnd/layouts/sidebar.blade.php
+++ resources/views/backEnd/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
       <div data-active-color="white" data-background-color="black" data-image="app-assets/img/sidebar-bg/01.jpg" class="app-sidebar">
         <div class="sidebar-header">
           <div class="logo clearfix"><a href="index.html" class="logo-text float-left">
-              <div class="logo-img"><img src="app-assets/img/logo.png" alt="Convex Logo"/></div><span class="text align-middle">BOOKSTORE</span></a><a id="sidebarToggle" href="javascript:;" class="nav-toggle d-none d-sm-none d-md-none d-lg-block"><i data-toggle="expanded" class="ft-disc toggle-icon"></i></a><a id="sidebarClose" href="javascript:;" class="nav-close d-block d-md-block d-lg-none d-xl-none"><i class="ft-circle"></i></a></div>
+              {{-- <div class="logo-img"><img src="app-assets/img/logo.png" alt="Convex Logo"/></div> --}}<span class="text align-middle">BOOKSTORE</span></a><a id="sidebarToggle" href="javascript:;" class="nav-toggle d-none d-sm-none d-md-none d-lg-block"><i data-toggle="expanded" class="ft-disc toggle-icon"></i></a><a id="sidebarClose" href="javascript:;" class="nav-close d-block d-md-block d-lg-none d-xl-none"><i class="ft-circle"></i></a></div>
         </div>
         <div class="sidebar-content">
           <div class="nav-container">
