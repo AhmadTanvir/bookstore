289f568e7cda61cde06441e10ca53222ee0821a0
AhmadTanvir <atasiddiq@gmail.com>
2019-07-21 10:19:24 +0600

    Update header.blade.php

--- resources/views/frontEnd/layouts/header.blade.php
+++ resources/views/frontEnd/layouts/header.blade.php
@@ -4,7 +4,7 @@
                 <div class="row">
                     <div class="col-md-2 col-sm-6 col-xs-6">
                         <div class="header-logo">
-                            <a href="index.html">
+                            <a href="{{url('/')}}">
                                 <img src="{{asset('frontEnd/imag/logo.png')}}" alt="">
                             </a>
                         </div>
@@ -12,9 +12,14 @@
                     <div class="col-md-1 col-sm-6 visible-sm  col-xs-6">
                         <div class="header-right">
                             <ul>
-                                <li>
-                                    <a href="account.html"><i class="flaticon-people"></i></a>
+                                @if(Auth::check())
+                                    <li><a href="{{ url('/logout') }}"><i class="fa fa-lock"></i>Logout</a>
                                 </li>
+                                @else
+                                    <li>
+                                        <a href="account.html"><i class="flaticon-people"></i></a>
+                                    </li>
+                                @endif
                                 <li class="shoping-cart">
                                     <a href="#">
                                         <i class="flaticon-shop"></i>
@@ -111,7 +116,7 @@
                         <div class="header-right">
                             <ul>
                                 <li>
-                                    <a href="#" data-toggle="modal" data-target="#myModal2"><i class="flaticon-people"></i></a>
+                                    <a href="#" data-toggle="modal" data-target="#myModal1"><i class="flaticon-people"></i></a>
                                 </li>
                                 <li class="shoping-cart">
                                     <a href="#">
@@ -182,6 +187,50 @@
             </div>
         </div>
         <!--Header Area End-->
+    <!-- Modal1 -->
+    <div class="modal fade" id="myModal1" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body modal-body-sub_agile">
+                    <div class="main-mailposi">
+                        <span class="fa fa-envelope-o" aria-hidden="true"></span>
+                    </div>
+                    <div class="modal_body_left modal_body_left1">
+                        <h3 class="agileinfo_sign">Sign In </h3>
+                        <p>
+                            Sign In now, Let's start your Grocery Shopping. Don't have an account?
+                            <a href="#" data-toggle="modal" data-target="#myModal2">
+                                Sign Up Now</a>
+                        </p>
+                        <form action="{{url('/user_login')}}" method="post">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                            <div class="styled-input agile-styled-input-top">
+                                <input type="email" placeholder="Email" name="email"/>
+                            </div>
+                            <div class="styled-input">
+                                <input type="password" placeholder="Password" name="password"/>
+                            </div>
+                            <div class="col-md-1">
+                                <input type="checkbox" class="checkbox">
+                            </div>
+                            <div class="col-md-4">
+                                <i>Remamber me</i>
+                            </div><br>
+                            <input type="submit" value="Sign In">
+                        </form>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+            <!-- //Modal content-->
+        </div>
+    </div>
+    <!-- //Modal1 -->
     <!-- Modal2 -->
     <div class="modal fade" id="myModal2" tabindex="-1" role="dialog">
         <div class="modal-dialog">
