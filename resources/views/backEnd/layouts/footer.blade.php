88cf196d07350aa6dbd14384b8e32c4a6143523f
AhmadTanvir <atasiddiq@gmail.com>
2019-07-30 13:13:47 +0600

    Update footer.blade.php

--- resources/views/backEnd/layouts/footer.blade.php
+++ resources/views/backEnd/layouts/footer.blade.php
@@ -1,4 +1,3 @@
-
-<footer class="footer footer-static footer-light">
-          <p class="clearfix text-muted text-center px-2"><span>Copyright  &copy; 2018 <a href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" id="pixinventLink" target="_blank" class="text-bold-800 primary darken-2">PIXINVENT </a>, All rights reserved. </span></p>
-</footer>
+<!-- <div class="row-fluid">
+    <div id="footer" class="span12">Copyright ©2015 Laravel5.7 (Cambodia). All rights reserved.</div>
+</div> -->
