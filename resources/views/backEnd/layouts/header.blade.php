e673a486cc242b1c52badb7d5aab7c828a610564
AhmadTanvir <atasiddiq@gmail.com>
2019-07-30 13:13:58 +0600

    Update header.blade.php

--- resources/views/backEnd/layouts/header.blade.php
+++ resources/views/backEnd/layouts/header.blade.php
@@ -1,31 +1,113 @@
-
-<!--Header-part-->
-<div id="header">
-    <h1><a href="dashboard.html">Laravel Admin</a></h1>
-</div>
-<!--close-Header-part-->
-<!--top-Header-menu-->
-<div id="user-nav" class="navbar navbar-inverse">
-    <ul class="nav">
-        <li class=""><a title="" href="{{url('/admin/settings')}}"><i class="icon icon-cog"></i> <span class="text">Settings</span></a></li>
-        <li class="">
-            <a class="dropdown-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                <i class="icon icon-share-alt"></i>{{ __('Logout') }}
-            </a>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-
-        </li>
-    </ul>
-</div>
-<!--close-top-Header-menu-->
-<!--start-top-serch-->
-<div id="search">
-    <input type="text" placeholder="Search here..."/>
-    <button type="submit" class="tip-bottom" title="Search"><i class="icon-search icon-white"></i></button>
-</div>
-<!--close-top-serch-->
+<div class="dash">
+        <div class="dash-nav dash-nav-dark">
+            <header>
+                <a href="#!" class="menu-toggle">
+                    <i class="fas fa-bars"></i>
+                </a>
+                <a href="index.html" class="spur-logo"><i class="fas fa-bolt"></i> <span>Spur</span></a>
+            </header>
+            <nav class="dash-nav-list">
+                <a href="index.html" class="dash-nav-item">
+                    <i class="fas fa-home"></i> Dashboard </a>
+                <div class="dash-nav-dropdown">
+                    <a href="#!" class="dash-nav-item dash-nav-dropdown-toggle">
+                        <i class="fas fa-list"></i> Categories </a>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="chartjs.html" class="dash-nav-dropdown-item">Add New Category</a>
+                    </div>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="chartjs.html" class="dash-nav-dropdown-item">List Categories</a>
+                    </div>
+                </div>
+                <div class="dash-nav-dropdown">
+                    <a href="#!" class="dash-nav-item dash-nav-dropdown-toggle">
+                        <i class="fas fa-list"></i> Products </a>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="{{url('/admin/product/create')}}" class="dash-nav-dropdown-item">Add New Products</a>
+                    </div>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="{{route('product.index')}}" class="dash-nav-dropdown-item">List Products</a>
+                    </div>
+                </div>
+                <div class="dash-nav-dropdown">
+                    <a href="#!" class="dash-nav-item dash-nav-dropdown-toggle">
+                        <i class="fas fa-list"></i> Coupons </a>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="chartjs.html" class="dash-nav-dropdown-item">Add New Coupon</a>
+                    </div>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="chartjs.html" class="dash-nav-dropdown-item">List Coupons</a>
+                    </div>
+                </div>
+                <div class="dash-nav-dropdown ">
+                    <a href="#!" class="dash-nav-item dash-nav-dropdown-toggle">
+                        <i class="fas fa-cube"></i> Components </a>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="cards.html" class="dash-nav-dropdown-item">Cards</a>
+                        <a href="forms.html" class="dash-nav-dropdown-item">Forms</a>
+                        <div class="dash-nav-dropdown ">
+                            <a href="#" class="dash-nav-dropdown-item dash-nav-dropdown-toggle">Icons</a>
+                            <div class="dash-nav-dropdown-menu">
+                                <a href="icons.html" class="dash-nav-dropdown-item">Solid Icons</a>
+                                <a href="icons.html#regular-icons" class="dash-nav-dropdown-item">Regular Icons</a>
+                                <a href="icons.html#brand-icons" class="dash-nav-dropdown-item">Brand Icons</a>
+                            </div>
+                        </div>
+                        <a href="stats.html" class="dash-nav-dropdown-item">Stats</a>
+                        <a href="tables.html" class="dash-nav-dropdown-item">Tables</a>
+                        <a href="typography.html" class="dash-nav-dropdown-item">Typography</a>
+                        <a href="userinterface.html" class="dash-nav-dropdown-item">User Interface</a>
+                    </div>
+                </div>
+                <div class="dash-nav-dropdown">
+                    <a href="#!" class="dash-nav-item dash-nav-dropdown-toggle">
+                        <i class="fas fa-file"></i> Layouts </a>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="blank.html" class="dash-nav-dropdown-item">Blank</a>
+                        <a href="content.html" class="dash-nav-dropdown-item">Content</a>
+                        <a href="login.html" class="dash-nav-dropdown-item">Log in</a>
+                        <a href="signup.html" class="dash-nav-dropdown-item">Sign up</a>
+                    </div>
+                </div>
+                <div class="dash-nav-dropdown">
+                    <a href="#!" class="dash-nav-item dash-nav-dropdown-toggle">
+                        <i class="fas fa-info"></i> About </a>
+                    <div class="dash-nav-dropdown-menu">
+                        <a href="https://github.com/HackerThemes/spur-template" target="_blank" class="dash-nav-dropdown-item">GitHub</a>
+                        <a href="http://hackerthemes.com" target="_blank" class="dash-nav-dropdown-item">HackerThemes</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="dash-app">
+            <header class="dash-toolbar">
+                <a href="#!" class="menu-toggle">
+                    <i class="fas fa-bars"></i>
+                </a>
+                <a href="#!" class="searchbox-toggle">
+                    <i class="fas fa-search"></i>
+                </a>
+                <form class="searchbox" action="#!">
+                    <a href="#!" class="searchbox-toggle"> <i class="fas fa-arrow-left"></i> </a>
+                    <button type="submit" class="searchbox-submit"> <i class="fas fa-search"></i> </button>
+                    <input type="text" class="searchbox-input" placeholder="type to search">
+                </form>
+                <div class="tools">
+                    <a href="https://github.com/HackerThemes/spur-template" target="_blank" class="tools-item">
+                        <i class="fab fa-github"></i>
+                    </a>
+                    <a href="#!" class="tools-item">
+                        <i class="fas fa-bell"></i>
+                        <i class="tools-item-count">4</i>
+                    </a>
+                    <div class="dropdown tools-item">
+                        <a href="#" class="" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-user"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
+                            <a class="dropdown-item" href="#!">Profile</a>
+                            <a class="dropdown-item" href="{{ url('logout') }}">Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
