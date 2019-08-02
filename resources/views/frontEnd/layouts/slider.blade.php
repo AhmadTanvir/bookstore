9a973afda7940b9a355e6bdbbeb2e8463db96a59
AhmadTanvir <atasiddiq@gmail.com>
2019-08-03 03:59:43 +0600

    Update slider.blade.php

--- resources/views/frontEnd/layouts/slider.blade.php
+++ resources/views/frontEnd/layouts/slider.blade.php
@@ -1,12 +1,75 @@
+<div> 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="{{asset('frontEnd/imag/slider/slide1.jpg')}}" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="{{asset('frontEnd/imag/slider/slide2.jpg')}}" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="{{asset('frontEnd/imag/slider/slide1.jpg')}}" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- slider Area Start -->
-        <div class="slider-area">
+        <!-- <div class="slider-area">
             <div class="bend niceties preview-1" id="slider-carousel">
                 <div id="ensign-nivoslider" class="slides"> 
                     <img src="{{asset('frontEnd/imag/slider/slide1.jpg')}}" alt="" title="#slider-direction-1"  />
                     <img src="{{asset('frontEnd/imag/slider/slide2.jpg')}}" alt="" title="#slider-direction-2"  />
                 </div>
             </div>
+        </div> -->
+<!-- <div id="wrapper">
+    <div class="slider-wrapper theme-default">
+        <div id="slider" class="nivoSlider"> 
+            <img src="{{asset('frontEnd/imag/slider/slide1.jpg')}}" data-thumb="{{asset('frontEnd/imag/slider/slide1.jpg')}}" alt="" /> 
+            <img src="{{asset('frontEnd/imag/slider/slide2.jpg')}}" data-thumb="{{asset('frontEnd/imag/slider/slide2.jpg')}}" alt="" title="This is an example of a caption" />
+            <img src="{{asset('frontEnd/imag/slider/slide1.jpg')}}" data-thumb="{{asset('frontEnd/imag/slider/slide1.jpg')}}" alt="" data-transition="slideInLeft" /> 
+            <img src="{{asset('frontEnd/imag/slider/slide2.jpg')}}" data-thumb="{{asset('frontEnd/imag/slider/slide2.jpg')}}" alt="" title="#htmlcaption" />
         </div>
+    </div>
+</div> -->
         <!-- slider Area End-->
 <!-- <section id="slider"> --><!--slider-->
     <!-- <div class="container">
