de83dc941921c0303b5fe222210915456fc932e5
AhmadTanvir <atasiddiq@gmail.com>
2019-08-02 11:47:37 +0600

    Update index.blade.php

--- resources/views/frontEnd/index.blade.php
+++ resources/views/frontEnd/index.blade.php
@@ -1,6 +1,22 @@
 @extends('frontEnd.layouts.master')
 @section('title','Home Page')
 @section('content')
+        <!-- <div class="container">
+            @if(session()->has('success_message'))
+                <div class="alert alert-success">
+                    {{ session()->get('success_message') }}
+                </div>
+            @endif
+            @if(count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div> -->
     <!-- Online Banner Area Start -->    
         <div class="online-banner-area">
             <div class="container">
