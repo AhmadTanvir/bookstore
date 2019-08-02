ca6fbf9d2693df428bde34876dd6ece91228ad60
AhmadTanvir <atasiddiq@gmail.com>
2019-08-03 04:00:07 +0600

    Update header.blade.php

--- resources/views/frontEnd/layouts/header.blade.php
+++ resources/views/frontEnd/layouts/header.blade.php
@@ -4,33 +4,42 @@
                 <div class="row">
                     <div class="col-md-2 col-sm-6 col-xs-6">
                         <div class="header-logo">
-                            <a href="{{url('/')}}">
-                                <img src="{{asset('frontEnd/imag/logo.png')}}" alt="">
-                            </a>
+                            
                         </div>
+                        <a style="padding-bottom: 0" href="{{url('/')}}">
+                            <img src="{{asset('frontEnd/imag/logo.png')}}" alt="">
+                        </a>
                     </div>
                     <div class="col-md-8 col-sm-12 hidden-xs">
                         <div class="mainmenu text-center">
                             <nav>
                                 <ul id="nav">
                                     <li><a href="{{url('/')}}">HOME</a></li>
+                                    <li><a href="#">pages</a>
+<!-- <?php
+    $categories=DB::table('categories')->where([['status',1],['parent_id',0]])->get();
+?> -->
+                                        <ul class="sub-menu">
+@foreach($categories as $category)
+<?php
+    $sub_categories=DB::table('categories')->select('id','name')->where([['parent_id',$category->id],['status',1]])->get();
+?>
+        <li>
+            <a href="{{route('cats',$category->id)}}">{{$category->name}}</a>
+            @if(count($sub_categories)>0)
+            <ul class="sub-menu" id="sportswear{{$category->id}}">
+                @foreach($sub_categories as $sub_category)
+                <li><a href="{{route('cats',$sub_category->id)}}">{{$sub_category->name}}</a></li>
+                @endforeach
+            </ul>
+            @endif
+        </li>
+@endforeach
+                                        </ul>
+                                    </li>
                                     <li><a href="{{url('/list-products')}}">FEATURED</a></li>
                                     <li><a href="shop.html">BOOK REVIEW</a></li>
                                     <li><a href="about.html">ABOUT AUTHOR</a></li>
-                                    <li><a href="#">pages</a>
-                                        <ul class="sub-menu">
-                                            <li><a href="about.html">About Us</a></li>
-                                            <li><a href="cart.html">Cart Page</a></li>
-                                            <li><a href="checkout.html">Check Out</a></li>
-                                            <li><a href="contact.html">Contact</a></li>
-                                            <li><a href="login.html">Login</a></li>
-                                            <li><a href="my-account.html">My Account</a></li>
-                                            <li><a href="shop.html">Shopping Page</a></li>
-                                            <li><a href="single-product.html">Single Shop Page</a></li>
-                                            <li><a href="wishlist.html">Wishlist Page</a></li>
-                                            <li><a href="404.html">404 Page</a></li>
-                                        </ul>
-                                    </li>
                                     <li><a href="contact.html">CONTACT</a></li>
                                 </ul>
                             </nav>
@@ -62,6 +71,41 @@
             </div>
         </div>
         <!--Header Area End-->
+        <!-- Mobile Menu Start -->
+        <div class="mobile-menu-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="mobile-menu">
+                            <nav id="dropdown">
+                                <ul>
+                                    <li><a href="index.html">HOME</a></li>
+                                    <li><a href="shop.html">FEATURED</a></li>
+                                    <li><a href="shop.html">REVIEW BOOK</a></li>
+                                    <li><a href="about.html">ABOUT AUTHOR</a></li>
+                                    <li><a href="#">pages</a>
+                                        <ul>
+                                            <li><a href="about.html">About Us</a></li>
+                                            <li><a href="cart.html">Cart Page</a></li>
+                                            <li><a href="checkout.html">Check Out</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="login.html">Login</a></li>
+                                            <li><a href="my-account.html">My Account</a></li>
+                                            <li><a href="shop.html">Shopping Page</a></li>
+                                            <li><a href="single-product.html">Single Shop Page</a></li>
+                                            <li><a href="wishlist.html">Wishlist Page</a></li>
+                                            <li><a href="404.html">404 Page</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">CONTACT</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>      
+        <!-- Mobile Menu End -->
     <!-- Modal1 -->
     <div class="modal fade" id="myModal1" tabindex="-1" role="dialog">
         <div class="modal-dialog">
