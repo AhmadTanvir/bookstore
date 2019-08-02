bf6a552205fda0cc5b6051d08e6fb0b60e7048f7
AhmadTanvir <atasiddiq@gmail.com>
2019-08-01 19:37:53 -0700

    Update slider.blade.php

--- resources/views/frontEnd/layouts/slider.blade.php
+++ resources/views/frontEnd/layouts/slider.blade.php
@@ -1,55 +1,9 @@
 <!-- slider Area Start -->
         <div class="slider-area">
-            <div class="bend niceties preview-1">
+            <div class="bend niceties preview-1" id="slider-carousel">
                 <div id="ensign-nivoslider" class="slides"> 
                     <img src="{{asset('frontEnd/imag/slider/slide1.jpg')}}" alt="" title="#slider-direction-1"  />
                     <img src="{{asset('frontEnd/imag/slider/slide2.jpg')}}" alt="" title="#slider-direction-2"  />
-                </div>
-                <!-- direction 1 -->
-                <div id="slider-direction-1" class="text-center slider-direction">
-                    <!-- layer 1 -->
-                    <div class="layer-1">
-                        <h2 class="title-1">LET’S WRITE IMAGINE</h2>
-                    </div>
-                    <!-- layer 2 -->
-                    <div class="layer-2">
-                        <p class="title-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                    <!-- layer 3 -->
-                    <div class="layer-3">
-                        <a href="#" class="title-3">SEE MORE</a>
-                    </div>
-                    <!-- layer 4 -->
-                    <div class="layer-4">
-                        <form action="{{ url('/search')}}" method="get" role="search" class="title-4 form-inline">
-                            {{ csrf_field() }}
-                            <input type="text" class="form-control" name="s" value="{{isset($s) ? $s : ''}}" placeholder="Enter your book title here">
-                            <button type="submit"><i class="fa fa-search"></i></button>
-                        </form>
-                    </div>
-                </div>
-                <!-- direction 2 -->
-                <div id="slider-direction-2" class="slider-direction">
-                    <!-- layer 1 -->
-                    <div class="layer-1">
-                        <h2 class="title-1">LET’S WRITE IMAGINE</h2>
-                    </div>
-                    <!-- layer 2 -->
-                    <div class="layer-2">
-                        <p class="title-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                    <!-- layer 3 -->
-                    <div class="layer-3">
-                        <a href="#" class="title-3">SEE MORE</a>
-                    </div>
-                    <!-- layer 4 -->
-                    <div class="layer-4">
-                        <form action="{{ url('/search')}}" method="get" role="search" class="title-4 form-inline">
-                            {{ csrf_field() }}
-                            <input type="text" class="form-control" name="s" value="{{isset($s) ? $s : ''}}" placeholder="Enter your book title here">
-                            <button type="submit"><i class="fa fa-search"></i></button>
-                        </form>
-                    </div>
                 </div>
             </div>
         </div>
